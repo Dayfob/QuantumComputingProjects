97ecf69f1008aefd9e94b95a5e92a693a0d3985e
Dayfob <semembaevalihan19@gmail.com>
2024-06-26 13:15:59 -0700

    Implemented working with qubits and gates.

---
+++ Project3/lab05_3/src/main/java/org/redfx/javaqc/App.java
@@ -0,0 +1,31 @@
+package org.redfx.javaqc;
+
+import org.redfx.strange.*;
+import org.redfx.strange.gate.*;
+import org.redfx.strange.local.*;
+import org.redfx.strangefx.render.Renderer;
+
+
+public class App {
+    public static void main(String[] args) {
+        QuantumExecutionEnvironment simulator = new SimpleQuantumExecutionEnvironment();
+        Program program = new Program(2);
+
+        Step step1 = new Step();
+        Step step2 = new Step();
+        step1.addGate(new X(0));
+        step1.addGate(new X(1));
+        step2.addGate(new X(0));
+        step2.addGate(new X(1));
+
+        program.addStep(step1);
+        program.addStep(step2);
+
+        Result result = simulator.runProgram(program);
+        Qubit[] qubits = result.getQubits();
+        Qubit zero = qubits[0];
+        int value = zero.measure();
+        System.out.println("Value is = " + value);
+        Renderer.renderProgram(program);
+    }
+}
